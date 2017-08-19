ab385a94f83050e553d366f34d8edd05e68ce970
KirCute <951206789@qq.com>
2017-08-19 21:05:45 +0900

    Support Minecraft - Stuff Library

--- src/VirtualChatBox.py
+++ src/VirtualChatBox.py
@@ -1,10 +1,51 @@
 import os
-from mcpi import minecraft
-from mcpi import block
+import socket
+import sys
+from mcpi import minecraft, block, vec3
 
-helpPageCount = 2
+disablems = False
+try:
+    import minecraftstuff
+except ImportError:
+    print '\033[0;33;40m',
+    print "Warning: Package \"minecraftstuff\" is defined."
+    print " It means some commands will not be available."
+    print " Try \"sudo pip install minecraftstuff\" to get it.",
+    print '\033[0m'
+    disablems = True
+
+try:
+    global mc
+    if len(sys.argv) == 3:
+        mc = minecraft.Minecraft.create(sys.argv[1], int(sys.argv[2]))
+    elif len(sys.argv) == 2:
+        mc = minecraft.Minecraft.create(sys.argv[1])
+    elif len(sys.argv) == 1:
+        mc = minecraft.Minecraft.create()
+    else:
+        raise TypeError("parameters are too many.")
+except ValueError:
+    print '\033[0;31;40m',
+    print "Parameter value error: ", sys.exc_info()[1],
+    print '\033[0m'
+    sys.exit(1)
+except TypeError:
+    print '\033[0;31;40m',
+    print "Parameter type error: ", sys.exc_info()[1],
+    print '\033[0m'
+    sys.exit(1)
+except socket.error, arg:
+    (errno, err_msg) = arg
+    print '\033[0;31;40m',
+    print "Cannot connect to server: %s, errno=%d" % (err_msg, errno),
+    print '\033[0m'
+    sys.exit(1)
+if not disablems:
+    mcdraw = minecraftstuff.MinecraftDrawing(mc)
+    mcshapes = {}
+    mcturtles = {}
+helpPageCount = 3
 playerName = "Player"
-mc = minecraft.Minecraft.create()
 blockDictionary = {
     "air": block.AIR,
     "stone": block.STONE,
@@ -153,6 +194,197 @@ blockDictionary = {
 }
 
 
+def draw(args):
+    if disablems:
+        print '\033[0;33;40m',
+        print "Package \"minecraftstuff\" is defined, \"/draw\" is not available.",
+        print '\033[0m'
+        return
+    if len(args) == 0:
+        invalid()
+        return
+    if args[0] == "circle":
+        if not (len(args) == 6 or len(args) == 7):
+            invalid()
+            return
+        vec = getvec(args[1], args[2], args[3], False)
+        radius = toint(args[4], 0, 64)
+        if radius == -1:
+            return
+        block = getblock(args[5])
+        if block == -1:
+            return
+        if len(args) == 7:
+            special = toint(args[4], 0, 15)
+            if special == -1:
+                return
+        else:
+            special = 0
+        mcdraw.drawCircle(vec[0], vec[1], vec[2], radius, block, special)
+        mc.postToChat("Drew circle successfully.")
+    elif args[0] == "face":
+        blocklist = []
+        if len(args) >= 7 and not((len(args) - 4) % 3):
+            filled = tobool(args[-3])
+            if filled == -1:
+                return
+            block = getblock(args[-2])
+            if block == -1:
+                return
+            special = toint(args[-1], 0, 15)
+            if special == -1:
+                return
+            times = (len(args) - 4) / 3
+        elif len(args) >= 6 and not((len(args) - 3) % 3):
+            filled = tobool(args[-2])
+            if filled == -1:
+                return
+            block = getblock(args[-1])
+            if block == -1:
+                return
+            special = 0
+            times = (len(args) - 3) / 3
+        else:
+            invalid()
+            return
+        time = 1
+        while time <= times:
+            veclist = getvec(args[time * 3 - 2], args[time * 3 - 1], args[time * 3], False)
+            if not veclist:
+                return
+            blocklist.append(vec3.Vec3(veclist[0], veclist[1], veclist[2]))
+            time += 1
+        mcdraw.drawFace(blocklist, filled, block, special)
+        mc.postToChat("Drew face successfully.")
+    elif args[0] == "hollowSphere":
+        if not (len(args) == 6 or len(args) == 7):
+            invalid()
+            return
+        vec = getvec(args[1], args[2], args[3], False)
+        radius = toint(args[4], 0, 64)
+        if radius == -1:
+            return
+        block = getblock(args[5])
+        if block == -1:
+            return
+        if len(args) == 7:
+            special = toint(args[4], 0, 15)
+            if special == -1:
+                return
+        else:
+            special = 0
+        mcdraw.drawHollowSphere(vec[0], vec[1], vec[2], radius, block, special)
+        mc.postToChat("Drew hollow sphere successfully.")
+    elif args[0] == "horizontalCircle":
+        if not (len(args) == 6 or len(args) == 7):
+            invalid()
+            return
+        vec = getvec(args[1], args[2], args[3], False)
+        radius = toint(args[4], 0, 64)
+        if radius == -1:
+            return
+        block = getblock(args[5])
+        if block == -1:
+            return
+        if len(args) == 7:
+            special = toint(args[4], 0, 15)
+            if special == -1:
+                return
+        else:
+            special = 0
+        mcdraw.drawHorizontalCircle(vec[0], vec[1], vec[2], radius, block, special)
+        mc.postToChat("Drew circle successfully.")
+    elif args[0] == "line":
+        if not(len(args) == 8 or len(args) == 9):
+            invalid()
+            return
+        vecB = getvec(args[1], args[2], args[3], False)
+        if not vecB:
+            return
+        vecE = getvec(args[4], args[5], args[6], False)
+        if not vecE:
+            return
+        block = getblock(args[7])
+        if block == -1:
+            return
+        if len(args) == 9:
+            special = toint(args[8], 0, 15)
+            if special == -1:
+                return
+        else:
+            special = 0
+        mcdraw.drawLine(vecB[0], vecB[1], vecB[2], vecE[0], vecE[1], vecE[2], block, special)
+        mc.postToChat("Drew line successfully.")
+    elif args[0] == "point":
+        if not (len(args) == 5 or len(args) == 6):
+            invalid()
+        else:
+            vec = getvec(args[1], args[2], args[3], False)
+            if not vec:
+                return
+            else:
+                if len(args) == 6:
+                    special = toint(args[5], 0, 15)
+                    if special == -1:
+                        return
+                else:
+                    special = 0
+                block = getblock(args[4])
+                if block == -1:
+                    return
+                mc.setBlock(vec[0], vec[1], vec[2], block, special)
+                mc.postToChat("Drew point successfully.")
+    elif args[0] == "sphere":
+        if not (len(args) == 6 or len(args) == 7):
+            invalid()
+            return
+        vec = getvec(args[1], args[2], args[3], False)
+        radius = toint(args[4], 0, 64)
+        if radius == -1:
+            return
+        block = getblock(args[5])
+        if block == -1:
+            return
+        if len(args) == 7:
+            special = toint(args[4], 0, 15)
+            if special == -1:
+                return
+        else:
+            special = 0
+        mcdraw.drawSphere(vec[0], vec[1], vec[2], radius, block, special)
+        mc.postToChat("Drew sphere successfully.")
+    elif args[0] == "vertices":
+        blocklist = []
+        if len(args) >= 6 and not ((len(args) - 3) % 3):
+            block = getblock(args[-2])
+            if block == -1:
+                return
+            special = toint(args[-1], 0, 15)
+            if special == -1:
+                return
+            times = (len(args) - 3) / 3
+        elif len(args) >= 5 and not ((len(args) - 2) % 3):
+            block = getblock(args[-1])
+            if block == -1:
+                return
+            special = 0
+            times = (len(args) - 2) / 3
+        else:
+            invalid()
+            return
+        time = 1
+        while time <= times:
+            veclist = getvec(args[time * 3 - 2], args[time * 3 - 1], args[time * 3], False)
+            if not veclist:
+                return
+            blocklist.append(vec3.Vec3(veclist[0], veclist[1], veclist[2]))
+            time += 1
+        mcdraw.drawVertices(blocklist, block, special)
+        mc.postToChat("Drew vertices successfully.")
+    else:
+        invalid()
+
+
 def setting(args):
     if not (len(args) == 0 or len(args) == 2):
         invalid()
@@ -160,12 +392,8 @@ def setting(args):
         if len(args) == 0:
             print "autojump, nametags_visible, world_immutable"
         else:
-            if args[1] == "true":
-                setting = True
-            elif args[1] == "false":
-                setting = False
-            else:
-                invalid()
+            setting = tobool(args[1])
+            if setting == -1:
                 return
             if args[0] == "autojump":
                 mc.player.setting("autojump", setting)
@@ -177,6 +405,7 @@ def setting(args):
                 invalid()
                 return
             mc.postToChat("Changed the " + args[0] + " setting to " + args[1])
+
 
 def clear(args):
     if not (len(args) == 0):
@@ -268,6 +497,22 @@ def help(args):
                 print "Usage:"
                 print " - /setting"
                 print " - /setting <setting: string> <status: boolean>"
+            elif args[0] == "draw":
+                print '\033[0;33;40m',
+                print "draw:"
+                print " Draws some shapes made of blocks."
+                if disablems:
+                    print '\033[0;33;40m', "Tip: this command is now unavailable."
+                print '\033[0m',
+                print "Usage:"
+                print " - /draw circle <position: x0 y0 z> <radius: int> <tileName: string | tileId: int> [tileData: int]"
+                print " - /draw face <<position1: x1 y1 z1> [position2: x2 y2 z2] [position3: x3 y3 z3] ...> <filled: boolean> <tileName: string | tileId: int> [tileData: int]"
+                print " - /draw hollowSphere <position: x y z> <radius: int> <tileName: string | tileId: int> [tileData: int]"
+                print " - /draw horizontalCircle <position: x0 y z0> <radius: int> <tileName: string | tileId: int> [tileData: int]"
+                print " - /draw line <position1: x1 y1 z1> <position2: x2 y2 z2> <tileName: string | tileId: int> [tileData: int]"
+                print " - /draw point <position: x y z> <tileName: string | tileId: int> [tileData: int]"
+                print " - /draw sphere <position: x y z> <radius: int> <tileName: string | tileId: int> [tileData: int]"
+                print " - /draw vertices <<position1: x1 y1 z1> [position2: x2 y2 z2] [position3: x3 y3 z3] ...> <tileName: string | tileId: int> [tileData: int]"
             else:
                 print '\033[0;31;40m',
                 print "The command is defined.",
@@ -275,22 +520,27 @@ def help(args):
         if list:
             print '\033[0;32;40m',
             print "--- Showing help page " + str(page) + " of " + str(helpPageCount) + " (/help <page>) ---"
-            print '\033[0m',
             if page == 1:
-                print "/clear"
-                print " /fill <from: x y z> <to: x y z> <tileName: string> [tileData: int]"
-                print " /fill <from: x y z> <to: x y z> <tileId: int> [tileData: int]"
-                print " /help <command: string>"
-                print " /help [page: int]"
+                print '\033[0m', "/clear"
+                print " /fill <from: x y z> <to: x y z> <tileName: string | tileId: int> [tileData: int]"
+                print " /help [command: string | page: int]"
                 print " /me <action: string>"
                 print " /say <message: string>"
-            elif page == 2:
-                print "/setblock <position: x y z> <tileName: string> [tileData: int]"
-                print " /setblock <position: x y z> <tileId: int> [tileData: int]"
+                print " /setblock <position: x y z> <tileName: string | tileId: int> [tileData: int]"
                 print " /setplayername [newPlayerName: string]"
-                print " /setting"
-                print " /setting <setting: string> <status: boolean>"
+            elif page == 2:
+                print '\033[0m', "/setting [<setting: string> <status: boolean>]"
                 print " /tp <destination: x y z>"
+                print '\033[0;33;40m', "/draw circle <position: x0 y0 z> <radius: int> <tileName: string | tileId: int> [tileData: int]", '\033[0m'
+                print '\033[0;33;40m', "/draw face <<position1: x1 y1 z1> [position2: x2 y2 z2] [position3: x3 y3 z3] ...> <filled: boolean> <tileName: string | tileId: int> [tileData: int]", '\033[0m'
+                print '\033[0;33;40m', "/draw hollowSphere <position: x y z> <radius: int> <tileName: string | tileId: int> [tileData: int]", '\033[0m'
+                print '\033[0;33;40m', "/draw horizontalCircle <position: x0 y z0> <radius: int> <tileName: string | tileId: int> [tileData: int]", '\033[0m'
+                print '\033[0;33;40m', "/draw line <position1: x1 y1 z1> <position2: x2 y2 z2> <tileName: string | tileId: int> [tileData: int]", '\033[0m'
+            elif page == 3:
+                print '\033[0;33;40m', "/draw point <position: x y z> <tileName: string | tileId: int> [tileData: int]", '\033[0m'
+                print '\033[0;33;40m', "/draw sphere <position: x y z> <radius: int> <tileName: string | tileId: int> [tileData: int]", '\033[0m'
+                print '\033[0;33;40m', "/draw vertices <<position1: x1 y1 z1> [position2: x2 y2 z2] [position3: x3 y3 z3] ...> <tileName: string | tileId: int> [tileData: int]", '\033[0m'
+            print '\033[0;32;40m', "Tip:", '\033[0;33;40m', "yellow", '\033[0;32;40m', "commands are add-ons' commands.", '\033[0m'
 
 
 def setplayername(args):
@@ -298,7 +548,7 @@ def setplayername(args):
         invalid()
     else:
         global playerName
-        if(len(args) == 1):
+        if len(args) == 1:
             playerName = args[0]
         else:
             playerName = "Player"
@@ -351,13 +601,10 @@ def setblock(args):
                     return
             else:
                 special = 0
-            if args[3] in blockDictionary.keys():
-                mc.setBlock(int(vec[0]), int(vec[1]), int(vec[2]), blockDictionary[args[3]], special)
-            else:
-                blockId = toint(args[3], 0, 256)
-                if blockId == -1:
-                    return
-                mc.setBlock(int(vec[0]), int(vec[1]), int(vec[2]), block.Block(blockId), special)
+            block = getblock(args[3])
+            if block == -1:
+                return
+            mc.setBlock(vec[0], vec[1], vec[2], block, special)
             mc.postToChat("Block placed")
 
 
@@ -372,19 +619,16 @@ def fill(args):
         if not vecE:
             return
         blockCount = (abs(vecB[0] - vecE[0]) + 1) * (abs(vecB[1] - vecE[1]) + 1) * (abs(vecB[2] - vecE[2]) + 1)
-        if (len(args) == 8):
+        if len(args) == 8:
             special = toint(args[7], 0, 15)
             if special == -1:
                 return
         else:
             special = 0
-        if (args[6] in blockDictionary.keys()):
-            mc.setBlocks(int(vecB[0]), int(vecB[1]), int(vecB[2]), int(vecE[0]), int(vecE[1]), int(vecE[2]), blockDictionary[args[6]], special)
-        else:
-            blockId = toint(args[6], 0, 256)
-            if (blockId == -1):
-                return
-            mc.setBlocks(int(vecB[0]), int(vecB[1]), int(vecB[2]), int(vecE[0]), int(vecE[1]), int(vecE[2]), block.Block(blockId), special)
+        block = getblock(args[6])
+        if block == -1:
+            return
+        mc.setBlocks(vecB[0], vecB[1], vecB[2], vecE[0], vecE[1], vecE[2], block, special)
         mc.postToChat(str(blockCount) + " blocks filled")
 
 
@@ -407,6 +651,8 @@ def switchcommand(args):
         clear(args[1:])
     elif args[0] == "setting":
         setting(args[1:])
+    elif args[0] == "draw":
+        draw(args[1:])
     else:
         print '\033[0;31;40m',
         print "Unknown command. Try /help for a list of commands",
@@ -431,6 +677,15 @@ def toomax(num, max):
     print '\033[0m'
 
 
+def getblock(argv):
+    if (argv in blockDictionary.keys()):
+        return blockDictionary[argv]
+    else:
+        blockId = toint(argv, 0, 256)
+        if (blockId == -1):
+            return -1
+
+
 def toint(intStr, min, max):
     try:
         res = int(intStr)
@@ -444,6 +699,16 @@ def toint(intStr, min, max):
         invalid()
         return -1
     return res
+
+
+def tobool(argv):
+    if argv == "true":
+        return True
+    elif argv == "false":
+        return False
+    else:
+        invalid()
+        return -1
 
 
 def getvec(x, y, z, isFloat):
@@ -497,7 +762,6 @@ def getvec(x, y, z, isFloat):
             except ValueError:
                 invalid()
                 return False
-        return vec
     else:
         vec = [0, 0, 0]
         if x[0] == "~":
@@ -509,7 +773,7 @@ def getvec(x, y, z, isFloat):
             except ValueError:
                 invalid()
                 return False
-            vec[0] = mc.player.getTilePos().x + add
+            vec[0] = int(mc.player.getTilePos().x) + add
         else:
             try:
                 vec[0] = int(x)
@@ -525,7 +789,7 @@ def getvec(x, y, z, isFloat):
             except ValueError:
                 invalid()
                 return False
-            vec[1] = mc.player.getTilePos().y + add
+            vec[1] = int(mc.player.getTilePos().y) + add
         else:
             try:
                 vec[1] = int(y)
@@ -541,14 +805,28 @@ def getvec(x, y, z, isFloat):
             except ValueError:
                 invalid()
                 return False
-            vec[2] = mc.player.getTilePos().z + add
+            vec[2] = int(mc.player.getTilePos().z) + add
         else:
             try:
                 vec[2] = int(z)
             except ValueError:
                 invalid()
                 return False
+    if vec[0] > 128:
+        toomax(vec[0], 128)
+    elif vec[0] < -128:
+        toomin(vec[0], -128)
+    elif vec[1] > 64:
+        toomax(vec[1], 64)
+    elif vec[1] < -64:
+        toomin(vec[1], -64)
+    elif vec[2] > 128:
+        toomax(vec[2], 128)
+    elif vec[2] < -128:
+        toomin(vec[2], -128)
+    else:
         return vec
+    return False
 
 
 while True:
@@ -560,12 +838,12 @@ while True:
     if not (input.find("/") == 0):
         mc.postToChat("<" + playerName + "> " + input)
         continue
-    args = []
-    argv = ""
+    commandArgs = []
+    addingArgv = ""
     for word in input[1:] + " ":
         if not (word == " "):
-            argv += word
+            addingArgv += word
         else:
-            args.append(argv)
-            argv = ""
-    switchcommand(args)
+            commandArgs.append(addingArgv)
+            addingArgv = ""
+    switchcommand(commandArgs)
